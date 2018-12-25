d7de13285af2c8db4bcfd82e6152bf1d661506af
(thatnaoki) <muroya.naoki@gmail.com>
2018-12-25 15:53:22 +0900

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Naoki Muroya on 2018/12/25.
+//  Copyright © 2018 Naoki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
